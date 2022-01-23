9a7dd647411204b07353d64e43d97d6e24ebfa1d
unknown <aavik743@gmail.com>
2022-01-23 17:27:02 +0530

    added; dice roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,10 +2,12 @@ package SnakeLadder;
 
 public class SnakeLadder
 {
+	//position variable
 	int apos = 0;
 	public static void main(String[] args) 
 	{
 		System.out.println("Snake and Laddar Simulation Game");
-		
+		int dieA = (int) Math.floor(Math.random()*10)%6 + 1;
+		System.out.println("Player A Rolled ! Die Value: "+dieA);
 	}
 }
