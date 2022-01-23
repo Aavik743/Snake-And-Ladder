580e01d5ed80247971c22b3a87d62d47676ae34e
unknown <aavik743@gmail.com>
2022-01-24 00:35:20 +0530

    added: current position and total number of rolls

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,23 +1,27 @@
 package SnakeLadder;
 
-public class SnakeLadder {
-    public static void main(String[] args) {
+public class SnakeLadder 
+{
+    public static void main(String[] args) 
+    {
         int apos = 0;
         int aflag = 0;
         int apos_temp;
-
+        int dieRolls = 0;
+       
         System.out.println("Snake and Laddar Simulation Game\n\n");
-        while (true) 
-        {
-			
-           int dieA = (int) Math.floor(Math.random() * 10) % 6 + 1;
-           if (aflag == 1) 
-           {
-              int lsvalueA = (int) Math.floor(Math.random() * 10) % 2 + 1;
-                
+        
+        while (true) {
+            dieRolls++;
+            
+            int dieA = (int) Math.floor(Math.random() * 10) % 6 + 1;
+           
+            if (aflag == 1) 
+            {
+                int lsvalueA = (int) Math.floor(Math.random() * 10) % 2 + 1;
                 switch (lsvalueA) 
                 {
-                    case 1: 
+                    case 1:
                     {
                         apos_temp = apos;
                         apos += dieA;
@@ -27,7 +31,7 @@ public class SnakeLadder {
                             System.out.println("\nPlayer A need to get Die value " + (100 - apos) + " to win\n");
                             break;
                         }
-                        System.out.println("\n Player A Rolled ! Die Value : " + dieA + "\n Player A Climbed Laddar with +" + dieA);
+                        System.out.println("\n Player A Rolled ! Die Value : " + dieA + "\n Player A Climbed Laddar with +" + dieA + "\n Player A's current position : " + apos + "\n");
                         break;
                     }
                     case 2: 
@@ -40,7 +44,7 @@ public class SnakeLadder {
                             System.out.println("\nOh ! Player A is Knocked Out . Need Die value 1 to get inside Game\n");
                             break;
                         }
-                        System.out.println("\n Player A Rolled ! Die Value : " + dieA + "\n Player A bitten by snake with -" + dieA );
+                        System.out.println("\n Player A Rolled ! Die Value : " + dieA + "\n Player A bitten by snake with -" + dieA + "\n Player A's current position : " + apos + "\n");
                         break;
                     }
                 }
@@ -50,13 +54,13 @@ public class SnakeLadder {
             {
                 aflag = 1;
                 apos = 1;
-                System.out.println("\nPlayer A is entered into Game");
+                System.out.println("\nPlayer A is entered into Game!! \n Player A's current position : " + apos + "\n");
             }
             if (apos >= 100) 
             {
-                System.out.println("\n Game Over !\n\n Player A wins");
+                System.out.println("\n Game Over !\n\n Player wins with " + dieRolls + " Die Rolls\n");
                 return;
             }
         }
     }
-}
+}   
