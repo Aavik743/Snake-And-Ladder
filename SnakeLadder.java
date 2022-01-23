052906da6a0f48b313853e371c8d672b2b08c7f0
unknown <aavik743@gmail.com>
2022-01-23 18:20:57 +0530

    added: options after die roll

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,12 +2,34 @@ package SnakeLadder;
 
 public class SnakeLadder
 {
-	//position variable
-	int apos = 0;
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
-		System.out.println("Snake and Laddar Simulation Game");
-		int dieA = (int) Math.floor(Math.random()*10)%6 + 1;
-		System.out.println("Player A Rolled ! Die Value: "+dieA);
+	
+	   {
+		   System.out.println(" Snake and Laddar Simulation Game ");
+		
+		   int dieA = (int) Math.floor(Math.random()*10) % 6 + 1;
+		   
+				System.out.println(" Player A Rolled ! Die Value: "+dieA);
+		
+				int isValueA = (int) Math.floor(Math.random()*10) % 3 + 1;
+				switch ( isValueA ) 
+				{
+					case 1 :
+						
+						System.out.println(" You climbed a ladder at: " +dieA);
+						break;
+		         
+					case 2 :
+						
+						System.out.println(" You got bitten by a snake at: " +dieA);
+						break;
+		         
+					default :
+						
+						System.out.println(" You hold your position at: " +dieA);
+			
+				}
+	   }
 	}
-}
+}	
