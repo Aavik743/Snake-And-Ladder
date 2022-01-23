a0f2ed723cd940a313578f7503863c11d92f4ccb
unknown <aavik743@gmail.com>
2022-01-23 16:58:19 +0530

    added: player position at start position = 0

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,8 +2,10 @@ package SnakeLadder;
 
 public class SnakeLadder
 {
+	int apos = 0;
 	public static void main(String[] args) 
 	{
 		System.out.println("Snake and Laddar Simulation Game");
+		
 	}
 }
